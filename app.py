968529892ba9d850f420af09960fa11868992bad
Will Rice <wrice20@gmail.com>
2020-11-06 10:10:36 -0500

    spokestack update

--- app.py
+++ app.py
@@ -1,54 +1,59 @@
-import logging
-
-from spokestack.activation_timeout import ActivationTimeout
-from spokestack.asr.speech_recognizer import CloudSpeechRecognizer
-from spokestack.io.pyaudio import PyAudioInput, PyAudioOutput
-from spokestack.nlu.tflite import TFLiteNLU
-from spokestack.pipeline import SpeechPipeline
-from spokestack.tts.clients.spokestack import TextToSpeechClient
-from spokestack.tts.manager import TextToSpeechManager
-from spokestack.vad.webrtc import VoiceActivityDetector
-from spokestack.wakeword.tflite import WakewordTrigger
+"""
+Main app
+"""
+from spokestack.activation_timeout import ActivationTimeout  # type: ignore
+from spokestack.asr.google.speech_recognizer import (  # type: ignore
+    GoogleSpeechRecognizer,
+)
+from spokestack.io.pyaudio import PyAudioInput, PyAudioOutput  # type: ignore
+from spokestack.pipeline import SpeechPipeline  # type: ignore
+from spokestack.tts.clients.spokestack import TextToSpeechClient  # type: ignore
+from spokestack.tts.manager import TextToSpeechManager  # type: ignore
+from spokestack.vad.webrtc import VoiceActivityDetector  # type: ignore
+from spokestack.wakeword.tflite import WakewordTrigger  # type: ignore
 
 from config import KEY_ID, KEY_SECRET
-from dialogue_manager import dialogue_manager
+from dialogue_manager import DialogueManager
 
 
 def main():
     pipeline = SpeechPipeline(
         PyAudioInput(frame_width=20, sample_rate=16000, exception_on_overflow=False),
         [
-            VoiceActivityDetector(vad_fall_delay=20),
+            VoiceActivityDetector(),
             WakewordTrigger(pre_emphasis=0.97, model_dir="tflite"),
-            CloudSpeechRecognizer(spokestack_id=KEY_ID, spokestack_secret=KEY_SECRET),
+            GoogleSpeechRecognizer("../spokestack-python/examples/google_asr.json"),
             ActivationTimeout(),
         ],
     )
 
-    nlu = TFLiteNLU("tflite")
+    dialogue_manager = DialogueManager(
+        "tflite", "distilbert-base-cased-distilled-squad"
+    )
     manager = TextToSpeechManager(
-        TextToSpeechClient(KEY_ID, KEY_SECRET), PyAudioOutput(),
+        TextToSpeechClient(KEY_ID, KEY_SECRET),
+        PyAudioOutput(),
     )
 
     @pipeline.event
     def on_activate(context):
-        print("active")
+        print(context.is_active)
 
     @pipeline.event
     def on_recognize(context):
         pipeline.pause()
-        results = nlu(context.transcript)
-        summary = dialogue_manager(results)
-        manager.synthesize(summary, "text", "demo-male")
+        answer = dialogue_manager(context.transcript)
+        manager.synthesize(answer, "text", "demo-male")
         pipeline.resume()
 
-    manager.synthesize(
-        "Welcome to wiki question answering, ask me about anything", "text", "demo-male"
-    )
+    @pipeline.event
+    def on_deactivate(context):
+        print(context.is_active)
+
+    manager.synthesize(dialogue_manager.greet(), "text", "demo-male")
     pipeline.start()
     pipeline.run()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     main()
--- dialogue_manager.py
+++ dialogue_manager.py
@@ -1,25 +1,79 @@
 """
 Simple QA dialogue manager
 """
-import re
 
-import wikipedia  # type: ignore
+import tensorflow as tf  # type: ignore
+from mediawiki import MediaWiki  # type: ignore
+from spokestack.nlu.result import Result
+from spokestack.nlu.tflite import TFLiteNLU
+from transformers import AutoTokenizer, TFAutoModelForQuestionAnswering  # type: ignore
 
 
-MATCH = r"\(.*\)[,\s]*"
+class DialogueManager:
+    """ Simple Question Answering Dialogue Manager """
 
+    def __init__(self, log_path: str, base_model: str) -> None:
+        self._wiki = MediaWiki()
+        self._entity_recognizer = TFLiteNLU(log_path)
+        self._tokenizer = AutoTokenizer.from_pretrained(base_model)
+        self._answerer = TFAutoModelForQuestionAnswering.from_pretrained(base_model)
 
-def dialogue_manager(results) -> str:
-    summary = "I do not know what that is."
-    if results.slots:
-        entity = results.slots.get("entity").get("raw_value")
-        if entity:
-            try:
-                summary = wikipedia.summary(entity, 1)
-                summary = re.sub(MATCH, "", summary)
-            except:
-                entity = wikipedia.search(entity)[0]
-                summary = wikipedia.summary(entity, 1, auto_suggest=False)
-                summary = re.sub(MATCH, "", summary)
+    def __call__(self, utterance: str) -> str:
+        result = self._entity_recognizer(utterance)
+        if result.intent == "ask.question":
+            return self._answer(result)
+        elif result.intent == "greet":
+            return self.greet()
+        elif result.intent == "command.exit":
+            return self.exit()
+        elif result.intent == "request.help":
+            return self.help()
+        else:
+            return self.fallback()
 
-    return summary
+    def _answer(self, result: Result) -> str:
+        if result.slots:
+            # get the tagged entity for page search
+            entity = result.slots.get("entity").get("raw_value")
+            # perform the search to find the wikipedia page
+            entity = self._wiki.search(entity)[0]
+            # get the page content to feed as context to the qa model
+            passage = self._wiki.page(entity, auto_suggest=False).content
+            # prepare qa model inputs
+            inputs = self._tokenizer(
+                result.utterance,
+                passage,
+                return_tensors="tf",
+                padding=True,
+                truncation=True,
+            )
+            # compute answer span
+            start_scores, end_scores = self._answerer(inputs)
+            start, end = tf.argmax(start_scores, -1)[0], tf.argmax(end_scores, -1)[0]
+            # prepare the passage ids for slicing
+            tokens = self._tokenizer.convert_ids_to_tokens(
+                (inputs["input_ids"].numpy()[0])
+            )
+            # retrieve only the answer from the passage
+            answer = self._tokenizer.convert_tokens_to_string(tokens[start : end + 1])
+            return answer
+        return "I don't have an answer for that"
+
+    @staticmethod
+    def greet() -> str:
+        return "Hello, Ask me anything"
+
+    @staticmethod
+    def exit() -> str:
+        return "Goodbye"
+
+    @staticmethod
+    def fallback() -> str:
+        return (
+            "I'm having trouble understanding your request, could you please "
+            "repeat it"
+        )
+
+    @staticmethod
+    def help() -> str:
+        return "Ask a question like, how long is the amazon river?"
